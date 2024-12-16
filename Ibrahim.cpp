a29e1589a6368d92d977577821b7bcd86d196ed6
Ibrahim-kamran-khan <u2024240@giki.edu.pk>
2024-12-16 17:08:21 +0500

    Create Ibrahim.cpp

---
+++ Ibrahim.cpp
@@ -0,0 +1,6 @@
+void checkAvailability() {
+    cout << "\nRoom Availability:\n";
+    cout << "1. Single Rooms: " << availableRooms[0] << " available\n";
+    cout << "2. Double Rooms: " << availableRooms[1] << " available\n";
+    cout << "3. Suite Rooms: " << availableRooms[2] << " available\n";
+}
